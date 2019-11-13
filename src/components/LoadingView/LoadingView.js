47a9835c9a7476cbf3f50ba4068917be1c188a2b
Alexis <alexis.noel@ynov.com>
2019-11-13 18:07:49 +0100

    Add complet component Voice & uninstall lottie-react-native react-native-animated-loader & install expo-speech & desactivate loader

--- src/components/LoadingView/LoadingView.js
+++ src/components/LoadingView/LoadingView.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import AnimatedLoader from "react-native-animated-loader";
+import { Text } from 'react-native';
+// import AnimatedLoader from "react-native-animated-loader";
 
 class LoadingView extends React.Component {
 	constructor(props) {
@@ -13,13 +14,14 @@ class LoadingView extends React.Component {
 	};
 	render() {
 		return (
-			<AnimatedLoader
-				visible={true}
-				overlayColor="rgba(255,255,255,0.75)"
-				source={require("./loader.json")}
-				animationStyle={{ width: 150, height: 150 }}
-				speed={1}
-			/>
+			// <AnimatedLoader
+			// 	visible={true}
+			// 	overlayColor="rgba(255,255,255,0.75)"
+			// 	source={require("./loader.json")}
+			// 	animationStyle={{ width: 150, height: 150 }}
+			// 	speed={1}
+			// />
+			<Text>Chargement...</Text>
 		);
 	};
 }
